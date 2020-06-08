6a96e5f14c2e532c4ea4a090770fa4f2ec276aa5
predator112 <mi460334@gmail.com>
2020-06-08 07:15:42 +0000

    add build.sh (gcc)

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=iqbal
+export KBUILD_BUILD_HOST=predator
+export CROSS_COMPILE=/home/iqbal/gcc64/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/iqbal/gcc32/bin/arm-linux-gnueabi-
+mkdir -p out
+make O=out clean
+make O=out mrproper
+make O=out ptedator_defconfig
+make O=out -j$(nproc --all)
