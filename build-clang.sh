4b7e84243556f6d7b7548ba77b77e6500dc39445
predator112 <65210380+predator112@users.noreply.github.com>
2020-07-06 12:31:41 +0700

    test 03 anjing

--- build-clang.sh
+++ build-clang.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# Copyright (C) 2020 Fiqri Ardyansyah (fiqri19102002)
 # Copyright (C) 2020 Mohammad Iqbal (predator112)
-# Configured for Redmi Note 6 Pro / tulip custom kernel source
+# Configured for Redmi Note 5 Pro / whyred custom kernel source
 # Simple Local Kernel Build Script
 
 # Clone AnyKernel
@@ -13,7 +12,7 @@ fi
 
 # Main Environment
 KERNEL_DIR=/home/loli/kernel
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+IMAGE=/home/loli/kernel/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=/home/loli/kernel/AnyKernel3
 CLANG_DIR=/home/loli/install
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
@@ -27,25 +26,29 @@ export ARCH=arm64
 export LD_LIBRARY_PATH="/home/loli/install/bin/../lib:$PATH"
 
 # compile plox
-make -C $(pwd) -j$(nproc) O=out predator_defconfig
-PATH="/home/loli/install/bin:${PATH}"
-make -j$(nproc) O=out ARCH=arm64 \
-       CC=clang \
-       CLANG_TRIPLE=aarch64-linux-gnu- \
-       CROSS_COMPILE=aarch64-linux-gnu- \
-       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-       O=out \
-       -j60 \
-       -l50 2>&1| tee build.log
-       if ! [ -a "$IMAGE" ]; then
-           finerr
-           exit 1
-         fi
- cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+function compile() {
+    make -C $(pwd) -j$(nproc) O=out predator_defconfig
+    PATH="/home/loli/install/bin:${PATH}"
+    make -j$(nproc) O=out ARCH=arm64 \
+           CC=clang \
+           CLANG_TRIPLE=aarch64-linux-gnu- \
+           CROSS_COMPILE=aarch64-linux-gnu- \
+           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+           O=out \
+           -j60 \
+           -l50 2>&1| tee build.log
+           if ! [ -a "$IMAGE" ]; then
+               finerr
+               exit 1
+           fi
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 }
-# Compress to zip file
-cd $ZIP_DIR
-make clean &>/dev/null
-make normal &>/dev/null
-cd ..
-echo -e "The build is complete, and is in the directory $ZIP_DIR"
+# Zipping
+function zipping() {
+    cd AnyKernel3 || exit 1
+    rm *.zip
+    if [ "$is_test" = true ]; then
+        zip -r9 Predator-TIREX-Kernel-whyred-"${TANGGAL}-${SHA}".zip * -x .git README.md *.zip
+    fi #ngentod
+    cd .. #well
+}
