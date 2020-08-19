eeb48c329f3e4162e77e83a967b83e664cdaf044
predator112 <mi460334@gmail.com>
2020-08-19 10:42:57 +0000

    fix derp

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -26,8 +26,6 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_driver_fast_switch(x, y) 0
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
-#define LATENCY_MULTIPLIER			(1000)
-
 #define UP_RATE_LIMIT_US			(1000)
 #define DOWN_RATE_LIMIT_US			(1000)
 #define SUGOV_KTHREAD_PRIORITY	50
