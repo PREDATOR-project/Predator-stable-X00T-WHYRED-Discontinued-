496101f9a70ac491b01aeffb2ae1665057b27090
predator112 <mi460334@gmail.com>
2020-09-02 21:54:28 +0000

    Revert "cpuidle: Mark CPUs idle as late as possible to avoid unneeded IPIs"
    This reverts commit 941bb376469b62103bfe72566755fb2d75a02b77.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -211,7 +211,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_start = ns_to_ktime(local_clock());
 
 	stop_critical_timings();
+	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
+	cpuidle_clear_idle_cpu(dev->cpu);
 	start_critical_timings();
 
 	sched_clock_idle_wakeup_event();
