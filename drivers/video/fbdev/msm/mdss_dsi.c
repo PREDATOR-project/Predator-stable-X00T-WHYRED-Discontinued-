7e2a40ccc0a6deb6ca2af67cd94e804c74d650f9
Mohammad Iqbal <mi460334@gmail.com>
2020-10-02 04:11:49 +0000

    Test

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -4264,7 +4264,8 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 
 	pdata = &ctrl_pdata->panel_data;
 	init_completion(&pdata->te_done);
-	if (!te_irq_registered) {
+        if (pdata->panel_info.type == MIPI_CMD_PANEL) {
+	        if (!te_irq_registered) {
 			rc = devm_request_irq(&pdev->dev,
 				gpio_to_irq(pdata->panel_te_gpio),
 				test_hw_vsync_handler, IRQF_TRIGGER_FALLING,
