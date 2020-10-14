c496c5c8a7532e24613edf8022842517fa3aa501
predator112 <65210380+predator112@users.noreply.github.com>
2020-10-14 16:03:23 +0700

    Test fix build

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1233,19 +1233,18 @@ static int reverse_path_check(void)
 
 static int ep_create_wakeup_source(struct epitem *epi)
 {
-	const char *name;
+	struct name_snapshot n;
 	struct wakeup_source *ws;
 
-	name = epi->ffd.file->f_path.dentry->d_name.name;
 	if (!epi->ep->ws) {
-		char buf[64];
-		snprintf(buf, sizeof(buf), "eventpoll pid:%d file:%s", current->pid, name);
-		epi->ep->ws = wakeup_source_register(buf);
+		epi->ep->ws = wakeup_source_register("eventpoll");
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
 
+	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
 	ws = wakeup_source_register(n.name);
+	release_dentry_name_snapshot(&n);
 
 	if (!ws)
 		return -ENOMEM;
